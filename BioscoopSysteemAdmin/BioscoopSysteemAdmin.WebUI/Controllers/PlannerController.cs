60b06c86f87d456c8c69c2d70cc26eff5f611494
rjkooijman <rjkooijman1995@gmail.com>
2015-03-26 00:50:40 +0100

    Show kan niet meer toegevoegd worden wanneer zaal bezet is

--- BioscoopSysteemAdmin/BioscoopSysteemAdmin.WebUI/Controllers/PlannerController.cs
+++ BioscoopSysteemAdmin/BioscoopSysteemAdmin.WebUI/Controllers/PlannerController.cs
@@ -14,34 +14,21 @@ namespace BioscoopSysteemAdmin.WebUI.Controllers {
 
         public PlannerController(IRepository repo) {
             this.repo = repo;
-        }
-
-        [HttpGet]
-        public ActionResult VoorstellingToevoegen() {
             List<Movie> moviesList = repo.GetAllMovies().Where(m => m.StartDate < DateTime.Now && m.EndDate > DateTime.Now).ToList();
             List<Room> roomList = repo.GetAllRooms().ToList();
             List<LadiesNight> ladiesNightList = repo.GetAllLadiesNights().Where(l => l.LadiesNightDay.DayOfYear > DateTime.Now.DayOfYear).ToList();
             List<Show> showList = repo.GetAllShows().Where(s => s.StartTime > DateTime.Now).ToList();
             List<SelectListItem> roomListSelect = new List<SelectListItem>();
             List<SelectListItem> movieListSelect = new List<SelectListItem>();
-            SelectListItem x = null;
-            foreach(Movie movie in moviesList) {
-                for (int i = 0; i < moviesList.Count(); i++) {
-                    x = new SelectListItem() { Text = movie.Name, Value = movie.MovieId.ToString() };
-                }
-                movieListSelect.Add(x);
-            }
-            foreach (Room room in roomList) {
-                for(int i = 0; i < roomList.Count(); i++) {
-                    x = new SelectListItem() { Text = room.RoomNumber.ToString(), Value = room.RoomNumber.ToString()};
-                }
-                roomListSelect.Add(x);
-            }
+        }
 
-            ViewBag.showList = showList;
+        [HttpGet]
+        public ActionResult VoorstellingToevoegen() {
+            List<LadiesNight> ladiesNightList = repo.GetAllLadiesNights().Where(l => l.LadiesNightDay.DayOfYear > DateTime.Now.DayOfYear).ToList();
+
             ViewBag.ladiesNightSelect = ladiesNightList;
-            ViewBag.RoomSelect = roomListSelect;
-            ViewBag.MovieSelect = movieListSelect;
+            ViewBag.RoomSelect = RoomSelect();
+            ViewBag.MovieSelect = MovieSelect();
             return View();
         }
 
@@ -49,8 +36,9 @@ namespace BioscoopSysteemAdmin.WebUI.Controllers {
         public ActionResult VoorstellingToevoegen(Show show) {
             DateTime date = DateTime.Parse(Request["datum"]);
             DateTime newDate = date.Add(TimeSpan.Parse(Request["starttijd"]));
-            show.StartTime = newDate;
             show.Movie = repo.GetMovieById(show.MovieId);
+            List<Room> roomList = repo.GetAllRooms().ToList();
+            List<Show> showList = repo.GetAllShows().Where(s => s.Room == repo.GetRoomById(show.RoomId)).ToList();
             show.Room = repo.GetRoomById(show.RoomId);
             if (repo.GetLadiesNightByDate(newDate) == null) {
                 show.LadiesNight = null;
@@ -59,10 +47,57 @@ namespace BioscoopSysteemAdmin.WebUI.Controllers {
                 show.LadiesNightid = show.LadiesNight.LadiesNightid;
             }
             
+            if(newDate.Date < show.Movie.EndDate.Date) {
+                foreach (Show x in showList) {
+                    Double doubleDuration = Double.Parse(show.Movie.Duration.ToString());
+                    TimeSpan span = TimeSpan.FromMinutes(doubleDuration);
+                   
+                    if (newDate.TimeOfDay > x.StartTime.TimeOfDay && newDate < (x.StartTime.AddMinutes(doubleDuration))) {
+                        List<LadiesNight> ladiesNightList = repo.GetAllLadiesNights().Where(l => l.LadiesNightDay.DayOfYear > DateTime.Now.DayOfYear).ToList();
+                        ViewBag.ladiesNightSelect = ladiesNightList;
+                        ViewBag.RoomSelect = RoomSelect();
+                        ViewBag.MovieSelect = MovieSelect();
+                        ViewBag.TimeError = "Deze zaal is bezet op de gegeven starttijd.";
+                        return View();
+                    }
+                }
+            } else {
+                List<LadiesNight> ladiesNightList = repo.GetAllLadiesNights().Where(l => l.LadiesNightDay.DayOfYear > DateTime.Now.DayOfYear).ToList();
+                ViewBag.ladiesNightSelect = ladiesNightList;
+                ViewBag.RoomSelect = RoomSelect();
+                ViewBag.MovieSelect = MovieSelect();
+                ViewBag.DateError = "De film is op deze dag niet meer beschikbaar.";
+            }
 
             repo.AddShow(show);
 
             return View("Index", "Home");
+        }
+
+        private List<SelectListItem> MovieSelect() {
+            List<SelectListItem> movieSelect = new List<SelectListItem>();
+            List<Movie> availableMovies = repo.GetAllMovies().Where(m => m.StartDate < DateTime.Now && m.EndDate > DateTime.Now).ToList();
+            SelectListItem x = null;
+            foreach (Movie movie in availableMovies) {
+                for (int i = 0; i < availableMovies.Count(); i++) {
+                    x = new SelectListItem() { Text = movie.Name, Value = movie.MovieId.ToString() };
+                }
+                movieSelect.Add(x);
+            }
+            return movieSelect;
+        }
+
+        private List<SelectListItem> RoomSelect() {
+            List<SelectListItem> roomSelect = new List<SelectListItem>();
+            List<Room> allRooms =  repo.GetAllRooms().ToList();
+             SelectListItem x = null;
+            foreach (Room room in allRooms) {
+                for (int i = 0; i < allRooms.Count(); i++) {
+                    x = new SelectListItem() { Text = room.RoomNumber.ToString(), Value = room.RoomNumber.ToString() };
+                }
+                roomSelect.Add(x);
+            }
+            return roomSelect;
         }
     }
 }
